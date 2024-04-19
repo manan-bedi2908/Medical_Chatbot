441bd5eb523e31aebb1cb9c29f6cdece636676a8
manan-bedi2908 <mananbedilps@gmail.com>
2024-04-20 01:20:29 +0530

    setup.py completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Manan Bedi',
+    author_email= 'mananbedi.tech@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
